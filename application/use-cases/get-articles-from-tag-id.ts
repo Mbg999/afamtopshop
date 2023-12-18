3bb3f2d5241cc30826e55a19c14db82ea46d816a
Mbg999 <miguelbg99@hotmail.com>
2023-12-18 11:03:29 +0100

    feat:  new get articles from tag id use case

---
+++ application/use-cases/get-articles-from-tag-id.ts
@@ -0,0 +1,12 @@
+import { ArticleTag } from "../../domain/article-tag.model.ts";
+import { articleTagRepositoryImpl } from "../../persistence/repositories/repositories.impl.ts";
+
+export class GetArticlesFromTagId {
+  constructor(
+    private readonly articleTagRepository = articleTagRepositoryImpl(),
+  ) {}
+
+  invoke(tagId: string): Promise<ArticleTag[]> {
+    return this.articleTagRepository.getArticlesFromTagId(tagId);
+  }
+}
--- persistence/repositories/repositories.impl.ts
+++ persistence/repositories/repositories.impl.ts
@@ -1,3 +1,9 @@
+import { Client, ClientConfig } from "../../deps.ts";
+import { ArticleTagRepository } from "./article-tag.repository.ts";
+import { ArticleRepository } from "./article.repository.ts";
+import { CategoryRepository } from "./category.repository.ts";
+import { DBBase } from "./db-base.ts";
+import { ImageRepository } from "./image.repository.ts";
 import { DBMySql } from "./mysql/db-mysql.ts";
 import { MySQLArticleTagRepository } from "./mysql/mysql-article-tag.repository.ts";
 import { MySQLArticleRepository } from "./mysql/mysql-article.repository.ts";
@@ -8,14 +14,30 @@ import { MySQLProductRepository } from "./mysql/mysql-product.repository.ts";
 import { MySQLShopProductRepository } from "./mysql/mysql-shop-product.repository.ts";
 import { MySQLShopRepository } from "./mysql/mysql-shop.repository.ts";
 import { MySQLTagRepository } from "./mysql/mysql-tag.repository.ts";
+import { ProductTagRepository } from "./product-tag.repository.ts";
+import { ProductRepository } from "./product.repository.ts";
+import { ShopProductRepository } from "./shop-product.repository.ts";
+import { ShopRepository } from "./shop.repository.ts";
+import { TagRepository } from "./tag.repository.ts";
 
-export const dbImpl = () => new DBMySql();
-export const categoryRepositoryImpl = () => new MySQLCategoryRepository();
-export const shopRepositoryImpl = () => new MySQLShopRepository();
-export const tagRepositoryImpl = () => new MySQLTagRepository();
-export const productRepositoryImpl = () => new MySQLProductRepository();
-export const articleRepositoryImpl = () => new MySQLArticleRepository();
-export const imageRepositoryImpl = () => new MySQLImageRepository();
-export const articleTagRepositoryImpl = () => new MySQLArticleTagRepository();
-export const productTagRepositoryImpl = () => new MySQLProductTagRepository();
-export const shopProductRepositoryImpl = () => new MySQLShopProductRepository();
+type DBType = DBMySql;
+
+export const dbImpl: () => DBBase<Client, ClientConfig> = () => new DBMySql();
+export const categoryRepositoryImpl: () => CategoryRepository<DBType> = () =>
+  new MySQLCategoryRepository();
+export const shopRepositoryImpl: () => ShopRepository<DBType> = () =>
+  new MySQLShopRepository();
+export const tagRepositoryImpl: () => TagRepository<DBType> = () =>
+  new MySQLTagRepository();
+export const productRepositoryImpl: () => ProductRepository<DBType> = () =>
+  new MySQLProductRepository();
+export const articleRepositoryImpl: () => ArticleRepository<DBType> = () =>
+  new MySQLArticleRepository();
+export const imageRepositoryImpl: () => ImageRepository<DBType> = () =>
+  new MySQLImageRepository();
+export const articleTagRepositoryImpl: () => ArticleTagRepository<DBType> =
+  () => new MySQLArticleTagRepository();
+export const productTagRepositoryImpl: () => ProductTagRepository<DBType> =
+  () => new MySQLProductTagRepository();
+export const shopProductRepositoryImpl: () => ShopProductRepository<DBType> =
+  () => new MySQLShopProductRepository();
